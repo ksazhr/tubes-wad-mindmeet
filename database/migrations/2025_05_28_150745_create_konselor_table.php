b7f7dc6df7b999e8f4460526a7fd7a1dde47d366
Kania Shafa <kaniashafa2801@gmail.com>
2025-05-28 23:01:05 +0700

    commit 10

--- database/migrations/2025_05_28_150745_create_konselor_table.php
+++ database/migrations/2025_05_28_150745_create_konselor_table.php
@@ -13,6 +13,10 @@ return new class extends Migration
     {
         Schema::create('konselor', function (Blueprint $table) {
             $table->id();
+            $table->string('nama');
+            $table->string('spesialisasi');
+            $table->string('email')->unique();
+            $table->string('password');
             $table->timestamps();
         });
     }
