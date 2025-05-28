10f810e693f7cda190e8b79b386e6b6253eb3935
meuthiariona <onariona690@gmail.com>
2025-05-28 22:54:09 +0700

    commit 6

---
+++ app/Models/Booking.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class Booking extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'nim',
+        'tanggal',
+        'status'
+    ];
+
+    // Relasi : Satu Mahasiswa bisa melakukan berkali-kali booking
+    public function Mahasiswa()
+    {
+        return $this->hasMany(Mahasiswa::class);
+    }
+
+    // Relasi : Satu jadwal hanya bisa booking satu kali
+    public function Jadwal()
+    {
+        return $this->belongsTo(Jadwal::class);
+    }
+}
