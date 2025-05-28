d5b9babfbe48d7d78eddde45d953aef8314b2211
Kania Shafa <kaniashafa2801@gmail.com>
2025-05-28 22:56:38 +0700

    commit 9

--- app/Models/Konselor.php
+++ app/Models/Konselor.php
@@ -2,9 +2,31 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Model;
 
 class Konselor extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = [
+        'nama',
+        'spesialisasi',
+        'email',
+        'password',
+    ];
+
+    use HasFactory;
+
+    // Relasi: Satu Konselor bisa dimiliki oleh banyak data Jadwal
+    public function Jadwal()
+    {
+        return $this->hasMany(Jadwal::class);
+    }
+
+    // Relasi: Satu Konselor bisa dimiliki oleh banyak data Feedback
+    public function Feedback()
+    {
+        return $this->hasMany(Feedback::class);
+    }
 }
