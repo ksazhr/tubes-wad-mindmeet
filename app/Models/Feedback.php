04262b2b520c6736e3bd4d889564656fcf48849b
Frixtho <frixtho.latumahina19@gmail.com>
2025-06-15 09:43:48 +0700

    commit 47

--- app/Models/Feedback.php
+++ app/Models/Feedback.php
@@ -12,19 +12,29 @@ class Feedback extends Model
         'komentar',
         'rating',
         'nim',
-        'konselor_id'
+        'konselor_id',
+        'booking_id',
     ];
 
     // Relasi: Satu Mahasiswa bisa membuat banyak Feedback
-    public function Mahasiswa()
+    public function mahasiswa() // Nama fungsi harus diawali huruf kecil sesuai konvensi
     {
-        return $this->hasMany(Mahasiswa::class);
+        return $this->belongsTo(Mahasiswa::class, 'nim', 'nim');
     }
 
-    // Relasi: Satu Konselor bisa melihat banyak Feedbcak
-    public function Konselor()
+    // Relasi: Setiap Feedback diberikan untuk satu Konselor
+    // Gunakan belongsTo karena 'konselor_id' ada di tabel feedbacks
+    public function konselor() // Nama fungsi harus diawali huruf kecil sesuai konvensi
     {
-        return $this->hasMany(Konselor::class);
+        return $this->belongsTo(Konselor::class);
+    }
+
+    // Relasi BARU yang hilang dan menyebabkan error "Undefined relationship [Booking]"
+    // Setiap Feedback terkait dengan satu Booking
+    // Gunakan belongsTo karena 'booking_id' ada di tabel feedbacks
+    public function booking() // Nama fungsi harus diawali huruf kecil sesuai konvensi
+    {
+        return $this->belongsTo(Booking::class);
     }
 }
 
